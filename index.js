5ac89e43d2705c518a6612e96d1972007e92112f
Alex Godwin <alexkgodwin@gmail.com>
2018-03-27 17:30:32 -0400

    Added status endpoint for uptime and version checking

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var cors = require('cors');
-var rpcController = require('./controller/controller.js')
+var rpcController = require('./controller/controller.js');
+var pjson = require('./package.json');
 
 var staticApp = express(); // Express app to serve static content
 staticApp.use(cors());
 
 staticApp.get('/content_bundle/', function(req, res) {
   return res.json(rpcController.getCDNData("bundle.json"));
+});
+
+staticApp.get('/status/', function(req, res){
+  return res.json({version: pjson.version, status: "up"});
 });
 
 rpcController.start(staticApp);
