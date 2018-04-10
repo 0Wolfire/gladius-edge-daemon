27902f44f8dd3cecd561b594b12201e1f9bef4e9
Alex Godwin <alexkgodwin@gmail.com>
2018-04-10 16:50:38 -0400

    Added start and stop methods to RPC control

--- cmd/gladius-edge-daemon/main.go
+++ cmd/gladius-edge-daemon/main.go
@@ -11,31 +11,36 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-// Create transport types
-type RPCData struct {
+// RPCOut - Transport struct
+type RPCOut struct {
+	httpState chan bool
 }
 
-type HTTPData struct {
+// HTTPOut - Transport struct
+type HTTPOut struct {
 }
 
+// GladiusEdge - Entry for the RPC interface. Methods take the form GladiusEdge.Method
 type GladiusEdge struct {
-	rpcData RPCData
+	rpcOut *RPCOut
 }
 
 // Start - Start the gladius edge node
-func (*GladiusEdge) Start(vals [2]int, res *string) error {
-	*res = "Not Implemented"
+func (g *GladiusEdge) Start(vals [2]int, res *string) error {
+	g.rpcOut.httpState <- true
+	*res = "Started the server"
 	return nil
 }
 
 // Stop - Stop the gladius edge node
-func (*GladiusEdge) Stop(vals [2]int, res *string) error {
-	*res = "Not Implemented"
+func (g *GladiusEdge) Stop(vals [2]int, res *string) error {
+	g.rpcOut.httpState <- false
+	*res = "Stopped the server"
 	return nil
 }
 
 // Status - Get the current status of the network node
-func (*GladiusEdge) Status(vals [2]int, res *string) error {
+func (g *GladiusEdge) Status(vals [2]int, res *string) error {
 	*res = "Not Implemented"
 	return nil
 }
@@ -56,18 +61,26 @@ func main() {
 func run() {
 	fmt.Println("Starting...")
 	// Create some strucs so we can pass info between goroutines
-	rpcData := &RPCData{}
-	// httpData := &HTTPData{}
+	rpcOut := &RPCOut{make(chan bool)}
+	httpOut := &HTTPOut{}
 
-	// Create some channels to communicate out of the threads
-	contentChannel, rpcChannel := make(chan string), make(chan string)
+	// Content server stuff below
 
-	// Create a content server goroutine
-	go fasthttp.ListenAndServe(":8080", requestHandler(contentChannel, rpcData))
+	// Listen on 8080
+	lnContent, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	// Create a content server
+	server := fasthttp.Server{Handler: requestHandler(httpOut)}
+	// Serve the content
+	defer lnContent.Close()
+	go server.Serve(lnContent)
+
+	// RPC Stuff below
 
 	// Register RPC methods
-	rpc.Register(&GladiusEdge{})
-
+	rpc.Register(&GladiusEdge{rpcOut: rpcOut})
 	// Setup HTTP handling for RPC on port 5000
 	http.Handle("/rpc", jsonrpc2.HTTPHandler(nil))
 	lnHTTP, err := net.Listen("tcp", ":5000")
@@ -82,22 +95,30 @@ func run() {
 	// Forever check through the channels on the main thread
 	for {
 		select {
-		case i := <-contentChannel: // If it can be assigned to a variable
-			fmt.Printf("it's a %q", i)
-		case i := <-rpcChannel: // If it can be assigned to a variable
-			fmt.Printf("it's a %q", i)
+		case state := <-(*rpcOut).httpState: // If it can be assigned to a variable
+			if state {
+				lnContent, err := net.Listen("tcp", ":8080")
+				if err != nil {
+					panic(err)
+				}
+				go server.Serve(lnContent)
+				fmt.Println("Started")
+			} else {
+				lnContent.Close()
+				fmt.Println("Stopped")
+			}
 		}
 	}
 }
 
 // Return a function like the one fasthttp is expecting
-func requestHandler(contentChannel chan string, rpcData *RPCData) func(ctx *fasthttp.RequestCtx) {
+func requestHandler(httpOut *HTTPOut) func(ctx *fasthttp.RequestCtx) {
 	// The actual serving function
 	return func(ctx *fasthttp.RequestCtx) {
 		switch string(ctx.Path()) {
 		case "/content":
 			contentHandler(ctx)
-			contentChannel <- "test" // Write to the channel (will likely be an int)
+			// TODO: Write stuff to pass back to httpOut
 		default:
 			ctx.Error("Unsupported path", fasthttp.StatusNotFound)
 		}
